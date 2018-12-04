edd348f848ef85b7518bf26ce99ea2bef15aac7d
Loic Denuziere <loic.denuziere@intellifactory.com>
2018-12-04 18:13:13 +0100

    Convert builder to netcore-based Paket

--- build.cmd
+++ build.cmd
@@ -1,11 +1,8 @@
 @echo off
 
-cd project
-
-dotnet restore
+if not exist .paket\fake.exe dotnet tool install fake-cli --tool-path .paket
+if not exist .paket\paket.exe dotnet tool install paket --tool-path .paket --version 5.189.1
 if errorlevel 1 exit /b %errorlevel%
 
-dotnet fake build %*
+.paket\fake build %*
 if errorlevel 1 exit /b %errorlevel%
-
-cd ..
--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-cd project
+EXE_EXT=
+if [ "$OS" = "Windows_NT" ]; then EXE_EXT=.exe; fi
+
+if ! [ -f ".paket/fake$EXE_EXT" ]; then dotnet tool install fake-cli --tool-path .paket; fi
+if ! [ -f ".paket/paket$EXE_EXT" ]; then dotnet tool install paket --tool-path .paket --version 5.189.1; fi
+
 PATH="~/.dotnet:$PATH"
-dotnet restore
-dotnet fake build "$@"
-cd ..
+.paket/fake build "$@"
