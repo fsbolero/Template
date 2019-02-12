8fab01705ac99742ce0dbed2f5fd8476b3964b26
Loic Denuziere <loic@denuziere.net>
2019-02-12 18:28:18 +0100

    Change paket version

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 if not exist .paket\fake.exe dotnet tool install fake-cli --tool-path .paket
-if not exist .paket\paket.exe dotnet tool install paket --tool-path .paket --version 5.193.0
+if not exist .paket\paket.exe dotnet tool install paket --tool-path .paket --version 5.196.2
 if errorlevel 1 exit /b %errorlevel%
 
 .paket\fake build %*
