2dcbccfce3eb40c8c04f12dd4fdc21365f0068a5
Loic Denuziere <loic@denuziere.net>
2019-09-23 23:45:41 +0200

    AppVeyor: build on Ubuntu

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
 
-EXE_EXT=
-if [ "$OS" = "Windows_NT" ]; then EXE_EXT=.exe; fi
+if [ "$OS" = "Windows_NT" ]; then EXE_EXT=.exe; else EXE_EXT=; fi
 
 if ! [ -f ".paket/fake$EXE_EXT" ]; then dotnet tool install fake-cli --tool-path .paket; fi
 if ! [ -f ".paket/nbgv$EXE_EXT" ]; then dotnet tool install nbgv --tool-path .paket; fi
-if ! [ -f ".paket/paket$EXE_EXT" ]; then dotnet tool install paket --tool-path .paket --version 5.206.0
+if ! [ -f ".paket/paket$EXE_EXT" ]; then dotnet tool install paket --tool-path .paket; fi
 
-PATH="~/.dotnet:$PATH"
 .paket/fake build "$@"
